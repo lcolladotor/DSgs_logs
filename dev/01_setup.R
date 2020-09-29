bb941a8e31a543f2e7bb41f3612385bd958f6787
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-09-29 00:05:37 -0400

    Add the same deploy build for the website

--- dev/01_setup.R
+++ dev/01_setup.R
@@ -4,3 +4,5 @@ dir.create("dev", showWarnings = FALSE)
 edit_file("dev/01_setup.R")
 
 use_git()
+dir.create(".github/workflows", recursive = TRUE)
+file.copy("../bioc_team_ds/.github/workflows/build_book_bioc_docker.yml", ".github/workflows/")
